e3a0f83df4e74a3dae7fb537f472dc19d32042ba
nextacademy <hyar001@e.ntu.edu.sg>
2016-07-25 12:37:07 +0800

    login?

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -1,33 +1,48 @@
-require 'byebug'
 enable :sessions
 
 get '/' do
-  erb :"static/index"
+	if logged_in?
+		erb :"static/profile"
+	else
+  		erb :"static/index"
+  	end
 end
 
 get '/register' do
-	session[:error] = []
+	if logged_in?
+		erb :"static/profile"
+	else
+  		session[:error] = []
 		erb :"static/signup"
+  	end
 end
 
 post '/signup' do
-	user = User.new(params[:user])
-	if user.save
-		redirect '/'
+	if logged_in?
+		erb :"static/profile"
 	else
-		session[:error]=user.errors.full_messages
-		erb :"static/signup"
-	end
+  		user = User.new(params[:user])
+		if user.save
+			redirect '/'
+		else
+			session[:error]=user.errors.full_messages
+			erb :"static/signup"
+		end
+  	end
 end
 
 post '/signin' do
-	login = User.find_by(email: params[:login][:email])
-	if login && login.authenticate(params[:login][:password])
-		session[:id] = login.id
+	if logged_in?
 		erb :"static/profile"
 	else
-		redirect '/'
-	end
+  		login = User.find_by(email: params[:login][:email])
+		if login && login.authenticate(params[:login][:password])
+			session[:id] = login.id
+			erb :"static/profile"
+		else
+			redirect '/'
+		end
+  	end
 end
 
 post '/signout' do
